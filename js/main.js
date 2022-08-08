2aeee11d15851ee101340f6e3f4b64d1f1c16ff2
Pouya Sadeqi <pouya.sdq@gmail.com>
2022-08-08 15:55:11 +0430

    complate all sections

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 const toggler = document.querySelector(".nav__toggler");
 const navbar = document.querySelector(".nav");
 toggler.addEventListener("click", (e) => {
-    navbar.classList.toggle("nav__expanded")
+  navbar.classList.toggle("nav__expanded");
 });
-
 
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
@@ -18,5 +17,14 @@ tabs.forEach((tab) => {
     // ADD NEW ACTIVE CLASSES
     tab.classList.add("active");
     targeTabContent.classList.add("active");
+  });
+});
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const accordion = document.querySelector(".accordion");
+accordionHeaders.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target.parentElement);
+    e.target.parentElement.classList.toggle("accordion__expanded");
   });
 });
